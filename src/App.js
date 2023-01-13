8bfd97df8c52a968b9dc8ad00a1424186af96dc7
Shubham Jaiswal <87111453+Shubham-Jaiswal-838@users.noreply.github.com>
2023-01-13 21:18:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Converter from "./Pages/Converter.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Converter />
+    </div>
+  );
+};
+export default App;
